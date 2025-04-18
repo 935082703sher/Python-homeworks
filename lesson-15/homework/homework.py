a4022ef99b7e7888b04ca7e8c9e863ea11f6f3fb
Sherzod <karomatovsherzod715@gmail.com>
2025-04-18 15:16:43 +0500

    Update homework.py

--- lesson-15/homework/homework.py
+++ lesson-15/homework/homework.py
@@ -4,7 +4,7 @@ with sqlite3.connect('SherzodbekK.db') as connection:
     query =' create table Roster( Name varchar(50), Species varchar(50), Age int )'
     query1 ="insert into Roster values ( 'Benjamin Sisko'	,'Human',	40 )"
     query2 ="insert into Roster values ( 'Jadzia Dax'	,'Trill',	300 )"
-    query3 ="insert into Roster values ( 'Kira Nerys'	,Bajoran',	29 )"
+    query3 ="insert into Roster values ( 'Kira Nerys'	,'Bajoran',	29 )"
     results =cursor.execute(query)
     r=cursor.execute(query1)
     r2=cursor.execute(query2)
